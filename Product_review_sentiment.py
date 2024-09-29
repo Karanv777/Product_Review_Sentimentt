fa6049ce29902763c8beed36f569bd66df2452df
Karan <162827087+Karanv777@users.noreply.github.com>
2024-09-29 14:35:25 +0530

    Add files via upload

---
+++ Product_review_sentiment.py
@@ -0,0 +1,32 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+sentiment = SentimentIntensityAnalyzer()
+
+while(True):
+    print("For review -- Press 1\n")
+    print("To exit -- Press any key\n")
+    x = input("Enter choice == ")
+
+    if x == '1':
+        text_1 = input("\nEnter your review for the product : ")
+
+        sent_1 = sentiment.polarity_scores(text_1)
+
+        try:
+            if sent_1["compound"] <= -0.5:
+                print("🤬😡\n")
+
+            elif sent_1["compound"] >= 0.5:
+                print("😍\n")
+
+            else:
+                print("😐\n")
+
+        except Exception as e:
+            print(e)
+
+    else:
+        print("\nProgram Closed!")
+        break        
+
+# import sys; print(sys.path)
